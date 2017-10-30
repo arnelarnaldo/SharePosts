d8c6f9ba04b23cc17b826440b4785b7af740da82
Arnel Arnaldo <arnela@columbia.ab.ca>
2017-10-30 13:45:43 -0600

    Pages, Bootstrap & Navbar

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -8,13 +8,15 @@
             
             $data =  [
                 'title' => 'SharePosts',
+                'description' => 'Simple social network built on the ArnaldoMVC PHP Framework'
             ];
             $this->view('pages/index', $data);
         }
 
         public function about(){
             $data =  [
-                'title' => 'About Us'
+                'title' => 'About Us',
+                'description' => 'App to share posts with other users'
             ];
             $this->view('pages/about', $data);
         }
